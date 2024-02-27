21f6c3968e19b5f01db274665641c65c7564235d
0rcaaa <iamsyarel@gmail.com>
2024-02-28 03:52:29 +0700

    +loading

--- src/js/index.js
+++ src/js/index.js
@@ -1,21 +1,23 @@
-function goToUrl(url, newTab = false) {
-  if (!newTab) {
-    window.location.href = "https://" + url;
-    return;
+(function() {
+  "use strict";
+    /**
+   * Easy selector helper function
+   */
+    const select = (el, all = false) => {
+      el = el.trim()
+      if (all) {
+        return [...document.querySelectorAll(el)]
+      } else {
+        return document.querySelector(el)
+      }
+    }
+  /**
+   * Preloader
+   */
+  let preloader = select('#preloader');
+  if (preloader) {
+    window.addEventListener('load', () => {
+      preloader.remove()
+    });
   }
-
-  window.open("https://" + url, "_blank");
-}
-
-// Tombol ham
-const navbarNav = document.querySelector('.navbar-nav');
-// onclick
-document.querySelector('#hambuger-menu').onclick = ( ) =>{navbarNav.classList.toggle('aktif');
-};
-
-const hambuger = document.querySelector('#hambuger-menu');
-document.addEventListener('click', function (e) {
-  if(!hambuger.contains(e.target)&&!navbarNav.contains(e.target)){
-    navbarNav.classList.remove('aktif');
-  }});
-// TODO: make data.json and fetch all content from data.json
+})();
