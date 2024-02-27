366251de24404eaaabbf22c6817351100433eebc
0rcaaa <iamsyarel@gmail.com>
2024-02-28 02:09:10 +0700

    tes

--- src/js/index.js
+++ src/js/index.js
@@ -7,4 +7,15 @@ function goToUrl(url, newTab = false) {
   window.open("https://" + url, "_blank");
 }
 
+// Tombol ham
+const navbarNav = document.querySelector('.navbar-nav');
+// onclick
+document.querySelector('#hambuger-menu').onclick = ( ) =>{navbarNav.classList.toggle('aktif');
+};
+
+const hambuger = document.querySelector('#hambuger-menu');
+document.addEventListener('click', function (e) {
+  if(!hambuger.contains(e.target)&&!navbarNav.contains(e.target)){
+    navbarNav.classList.remove('aktif');
+  }});
 // TODO: make data.json and fetch all content from data.json
