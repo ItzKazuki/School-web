d7eadf9cfa323b318ff5be1ce671443d362ea1fb
Kazuki <ibnu235729@gmail.com>
2024-03-02 17:59:21 +0000

    features(fix navbar): fix navbar when scroll add background color

--- src/js/index.js
+++ src/js/index.js
@@ -21,3 +21,18 @@
     });
   }
 })();
+
+window.addEventListener('scroll', () => {
+  let news = document.getElementById('news');
+  let navbar = document.getElementById('navbar');
+  let scrollTop = window.scrollY;
+
+  if(scrollTop <= news.offsetTop) {
+    navbar.classList.remove("bg-brown-800");
+  }
+
+  if(scrollTop >= news.offsetTop) {
+    // console.log('udh di bawah banh')
+    navbar.classList.add("bg-brown-800");
+  }
+})
